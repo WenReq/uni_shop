806cc8c8f357566e293bdd311232df79d2971a1f
wenreq <294491328@qq.com>
2022-10-04 21:03:52 +0800

    feat(购物车)：结算区域开发完毕

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,5 +1,5 @@
 <template>
-	<view>
+	<view class="cart-container" v-if="cart.length !== 0">
 		<my-address></my-address>
 		<!-- 购物车商品列表的标题区域 -->
 		<view class="cart-title">
@@ -18,7 +18,13 @@
 		    </uni-swipe-action-item>
 		  </block>
 		</uni-swipe-action>
+		<my-settle></my-settle>
 	</view>
+	<!-- 空白购物车区域 -->
+	  <view class="empty-cart" v-else>
+	    <image src="/static/empty.svg" class="empty-img"></image>
+	    <text class="tip-text">空空如也~</text>
+	  </view>
 </template>
 
 <script>
@@ -66,6 +72,9 @@
 </script>
 
 <style lang="scss">
+	.cart-container {
+		padding-bottom: 50px;
+	}
 	.cart-title {
 		height: 40px;
 		display: flex;
@@ -77,5 +86,22 @@
 		.cart-title-text {
 			margin-left: 10px;
 		}
+	}
+	.empty-cart {
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	  padding-top: 150px;
+	
+	  .empty-img {
+	    width: 90px;
+	    height: 90px;
+	  }
+	
+	  .tip-text {
+	    font-size: 12px;
+	    color: gray;
+	    margin-top: 15px;
+	  }
 	}
 </style>
