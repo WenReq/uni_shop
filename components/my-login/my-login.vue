fb59905a51a5af312dad5f8f7a074d1b8cc5a28c
wenreq <294491328@qq.com>
2022-10-08 20:53:20 +0800

    feat:用户信息和三秒后自动跳转的开发

--- components/my-login/my-login.vue
+++ components/my-login/my-login.vue
@@ -13,7 +13,8 @@
 <script>
 	// 1. 按需导入 mapMutations 辅助函数
 	import {
-		mapMutations
+		mapMutations,
+		mapState,
 	} from 'vuex'
 
 	export default {
@@ -23,9 +24,13 @@
 
 			};
 		},
+		computed: {
+			// 调用 mapState 辅助方法，把 m_user 模块中的数据映射到当前用组件中使用
+			...mapState('m_user', ['redirectInfo']),
+		},
 		methods: {
 			// 2. 调用 mapMutations 辅助方法，把 m_user 模块中的 updateUserInfo、updateToken 映射到当前组件中使用
-			...mapMutations('m_user', ['updateUserInfo', 'updateToken']),
+			...mapMutations('m_user', ['updateUserInfo', 'updateToken', 'updateRedirectInfo']),
 			// 获取微信用户的基本信息
 			getUserInfo(e) {
 				// 判断是否获取用户信息成功
@@ -44,7 +49,6 @@
 				// 调用微信登录接口
 				const [err, res] = await uni.login().catch(err => err)
 				// 判断是否 uni.login() 调用失败
-				debugger
 				if (err || res.errMsg !== 'login:ok') return uni.$showError('登录失败！')
 
 				// 准备参数对象
@@ -60,12 +64,31 @@
 				const {
 					data: loginResult
 				} = await uni.$http.post('/api/public/v1/users/wxlogin', query)
-				debugger
 				if (loginResult.meta.status !== 400) return uni.$showMsg('登录失败！')
 				uni.$showMsg('登录成功')
-				
+
 				// 更新 vuex 中的 token
-				this.updateToken(loginResult.message.token)
+				// this.updateToken(loginResult.message.token)
+				this.updateToken(query.signature)
+
+				// 判断 vuex 中的 redirectInfo 是否为 null
+				// 如果不为 null，则登录成功之后，需要重新导航到对应的页面
+				this.navigateBack()
+			},
+			// 返回登录之前的页面
+			navigateBack() {
+				// redirectInfo 不为 null，并且导航方式为 switchTab
+				if (this.redirectInfo && this.redirectInfo.openType === 'switchTab') {
+					// 调用小程序提供的 uni.switchTab() API 进行页面的导航
+					uni.switchTab({
+						// 要导航到的页面地址
+						url: this.redirectInfo.from,
+						// 导航成功之后，把 vuex 中的 redirectInfo 对象重置为 null
+						complete: () => {
+							this.updateRedirectInfo(null)
+						}
+					})
+				}
 			}
 		}
 	}
--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -38,11 +38,16 @@
 		},
 		data() {
 			return {
-
+				// 倒计时的秒数
+				seconds: 3,
+				// 定时器的 Id
+				timer: null
 			};
 		},
 		methods: {
 			...mapMutations('m_cart', ['updateAllGoodsState']),
+			// 把 m_user 模块中的 updateRedirectInfo 方法映射到当前页面中使用
+			...mapMutations('m_user', ['updateRedirectInfo']),
 			// label 的点击事件处理函数
 			changeAllState() {
 				// 修改购物车中所有商品的选中状态
@@ -53,12 +58,65 @@
 			settlement() {
 				// 1. 先判断是否勾选了要结算的商品
 				if (!this.checkedCount) return uni.$showMsg('请选择要结算的商品！')
-			
+
 				// 2. 再判断用户是否选择了收货地址
 				if (!this.addstr) return uni.$showMsg('请选择收货地址！')
-			
-				// 3. 最后判断用户是否登录了
-				if (!this.token) return uni.$showMsg('请先登录！')
+
+				// 3. 最后判断用户是否登录了，如果没有登录，则调用 delayNavigate() 进行倒计时的导航跳转
+				// if (!this.token) return uni.$showMsg('请先登录！')
+				if (!this.token) return this.delayNavigate()
+			},
+			// 延迟导航到 my 页面
+			delayNavigate() {
+				// 1. 展示提示消息，此时 seconds 的值等于 3
+				// 把 data 中的秒数重置成 3 秒
+				this.seconds = 3
+				this.showTips(this.seconds)
+
+				// 1. 将定时器的 Id 存储到 timer 中
+				this.timer = setInterval(() => {
+					this.seconds--
+
+					// 2. 判断秒数是否 <= 0
+					if (this.seconds <= 0) {
+						// 2.1 清除定时器
+						clearInterval(this.timer)
+
+						// 2.2 跳转到 my 页面
+						uni.switchTab({
+							url: '/pages/my/my',
+							// 页面跳转成功之后的回调函数
+							        success: () => {
+							          // 调用 vuex 的 updateRedirectInfo 方法，把跳转信息存储到 Store 中
+							          this.updateRedirectInfo({
+							            // 跳转的方式
+							            openType: 'switchTab',
+							            // 从哪个页面跳转过去的
+							            from: '/pages/cart/cart'
+							          })
+							        }
+						})
+
+						// 2.3 终止后续代码的运行（当秒数为 0 时，不再展示 toast 提示消息）
+						return
+					}
+
+					this.showTips(this.seconds)
+				}, 1000)
+			},
+			// 展示倒计时的提示消息
+			showTips(n) {
+				// 调用 uni.showToast() 方法，展示提示消息
+				uni.showToast({
+					// 不展示任何图标
+					icon: 'none',
+					// 提示的消息
+					title: '请登录后再结算！' + n + ' 秒后自动跳转到登录页',
+					// 为页面添加透明遮罩，防止点击穿透
+					mask: true,
+					// 1.5 秒后自动消失
+					duration: 1500
+				})
 			}
 		}
 	}
